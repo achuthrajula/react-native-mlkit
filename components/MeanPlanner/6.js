8a9cc0c77bd68ebcc82561212de4b0f9de9cf563
achuthrajula <achuthrajula1999@gmail.com>
2020-07-24 17:25:05 +0530

    Material UI update (3/3)

--- components/MeanPlanner/6.js
+++ components/MeanPlanner/6.js
@@ -2,6 +2,29 @@ import * as React from 'react';
 import { Text, View, Image, Button, ScrollView } from 'react-native';
 
 function HomeScreen({ navigation }) {
+    const insights = ["29 Minutes", "17 Ingredients", "2 Ingredients"]
+    let holder = []
+    const dummy = () => {
+        for (let i = 0; i < 20; i++) {
+          holder.push(
+          <View key={i} style={{ flexDirection: 'row', margin: 5, backgroundColor:  "#FFE50061", height: 60 }}>
+                <View style={{ justifyContent: 'center', alignItems: 'center',  width: 100 }}>
+                    <Image
+                    source={require('../../assets/breakfast.png')}
+                />
+                </View>
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start' }}>
+                    <Text>Breakfast</Text>
+                </View>
+                <View style={{justifyContent: 'center', alignItems: 'center' }}>
+                    <Image 
+                    source={require('../../assets/add.png')}
+                    />
+                </View>
+            </View>
+        )}
+        return holder;
+      };
     return (
       <View style={{flex: 1, flexDirection: 'column'}}>
             <View style={{ flex: 1, alignItems: 'center' }}>
@@ -20,36 +43,20 @@ function HomeScreen({ navigation }) {
                 <Text style={{ fontSize: 40 }}>Veg Noodles</Text>
             </View>
             <View flexDirection="row" style={{ alignItems: "center", justifyContent: "center", borderStyle: "dashed", borderTopWidth: 2, borderBottomWidth: 2, borderColor: "#FF5800"}} >
-                <View style={{ margin: 3 }}>
-                    <View style={{ height: 60, width: 60, borderRadius: 30, borderWidth: 2, borderColor: "#FF5800", justifyContent: 'center', alignItems: 'center' }}>
-                        {/* <Image
-                            source={require('../../assets/clock.png')}
-                        /> */}
-                    </View>
-                    <View>
-                        <Text style={{ margin: 10, justifyContent: 'center', alignItems: 'center' }}>29 Minutes</Text>
-                    </View>
-                </View>
-                <View style={{ margin: 3 }}>
-                    <View style={{ height: 60, width: 60, borderRadius: 30, borderWidth: 2, borderColor: "#FF5800", justifyContent: 'center', alignItems: 'center' }}>
-                        {/* <Image
-                            source={require('../../assets/bag.png')}
-                        /> */}
-                    </View>
-                    <View>
-                        <Text style={{ margin: 10, justifyContent: 'center', alignItems: 'center' }}>17 Ingredients</Text>
-                    </View>
-                </View>
-                <View style={{ margin: 3 }}>
-                    <View style={{ height: 60, width: 60, borderRadius: 30, borderWidth: 2, borderColor: "#FF5800", justifyContent: 'center', alignItems: 'center' }}>
-                        {/* <Image
-                            source={require('../../assets/shop.png')}
-                        /> */}
-                    </View>
-                    <View>
-                        <Text style={{ margin: 10, justifyContent: 'center', alignItems: 'center' }}>2 Ingredients</Text>
-                    </View>
-                </View>
+                {insights.map(insight => {
+                    return(
+                        <View key={insight} style={{ margin: 3 }}>
+                            <View style={{ height: 60, width: 60, borderRadius: 30, borderWidth: 2, borderColor: "#FF5800", justifyContent: 'center', alignItems: 'center' }}>
+                                {/* <Image
+                                    source={require('../../assets/clock.png')}
+                                /> */}
+                            </View>
+                            <View>
+                            <Text style={{ margin: 10, justifyContent: 'center', alignItems: 'center' }}>{insight}</Text>
+                            </View>
+                        </View>
+                    )
+                })}
           </View>
           <Text style={{ fontSize: 20, margin: 10 }}>Ideal Servings: 4</Text>
           <View style={{ height: 200 }}>
@@ -58,276 +65,7 @@ function HomeScreen({ navigation }) {
                 viewabilityConfig={{
                     itemVisiblePercentThreshold: 50
                 }}>
-                <View style={{ flexDirection: 'row', margin: 5, backgroundColor:  "#FFE50061", height: 60 }}>
-                    <View style={{ justifyContent: 'center', alignItems: 'center',  width: 100 }}>
-                        <Image
-                        source={require('../../assets/breakfast.png')}
-                    />
-                    </View>
-                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start' }}>
-                        <Text>Breakfast</Text>
-                    </View>
-                    <View style={{justifyContent: 'center', alignItems: 'center' }}>
-                        <Image 
-                        source={require('../../assets/add.png')}
-                        />
-                    </View>
-                </View>
-                <View style={{ flexDirection: 'row', margin: 5, backgroundColor:  "#FFE50061", height: 60 }}>
-                    <View style={{ justifyContent: 'center', alignItems: 'center',  width: 100 }}>
-                        <Image
-                        source={require('../../assets/breakfast.png')}
-                    />
-                    </View>
-                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start' }}>
-                        <Text>Breakfast</Text>
-                    </View>
-                    <View style={{justifyContent: 'center', alignItems: 'center' }}>
-                        <Image 
-                        source={require('../../assets/add.png')}
-                        />
-                    </View>
-                </View>
-                <View style={{ flexDirection: 'row', margin: 5, backgroundColor:  "#FFE50061", height: 60 }}>
-                    <View style={{ justifyContent: 'center', alignItems: 'center',  width: 100 }}>
-                        <Image
-                        source={require('../../assets/breakfast.png')}
-                    />
-                    </View>
-                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start' }}>
-                        <Text>Breakfast</Text>
-                    </View>
-                    <View style={{justifyContent: 'center', alignItems: 'center' }}>
-                        <Image 
-                        source={require('../../assets/add.png')}
-                        />
-                    </View>
-                </View>
-                <View style={{ flexDirection: 'row', margin: 5, backgroundColor:  "#FFE50061", height: 60 }}>
-                    <View style={{ justifyContent: 'center', alignItems: 'center',  width: 100 }}>
-                        <Image
-                        source={require('../../assets/breakfast.png')}
-                    />
-                    </View>
-                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start' }}>
-                        <Text>Breakfast</Text>
-                    </View>
-                    <View style={{justifyContent: 'center', alignItems: 'center' }}>
-                        <Image 
-                        source={require('../../assets/add.png')}
-                        />
-                    </View>
-                </View>
-                <View style={{ flexDirection: 'row', margin: 5, backgroundColor:  "#FFE50061", height: 60 }}>
-                    <View style={{ justifyContent: 'center', alignItems: 'center',  width: 100 }}>
-                        <Image
-                        source={require('../../assets/breakfast.png')}
-                    />
-                    </View>
-                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start' }}>
-                        <Text>Breakfast</Text>
-                    </View>
-                    <View style={{justifyContent: 'center', alignItems: 'center' }}>
-                        <Image 
-                        source={require('../../assets/add.png')}
-                        />
-                    </View>
-                </View>
-                <View style={{ flexDirection: 'row', margin: 5, backgroundColor:  "#FFE50061", height: 60 }}>
-                    <View style={{ justifyContent: 'center', alignItems: 'center',  width: 100 }}>
-                        <Image
-                        source={require('../../assets/breakfast.png')}
-                    />
-                    </View>
-                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start' }}>
-                        <Text>Breakfast</Text>
-                    </View>
-                    <View style={{justifyContent: 'center', alignItems: 'center' }}>
-                        <Image 
-                        source={require('../../assets/add.png')}
-                        />
-                    </View>
-                </View>
-                <View style={{ flexDirection: 'row', margin: 5, backgroundColor:  "#FFE50061", height: 60 }}>
-                    <View style={{ justifyContent: 'center', alignItems: 'center',  width: 100 }}>
-                        <Image
-                        source={require('../../assets/breakfast.png')}
-                    />
-                    </View>
-                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start' }}>
-                        <Text>Breakfast</Text>
-                    </View>
-                    <View style={{justifyContent: 'center', alignItems: 'center' }}>
-                        <Image 
-                        source={require('../../assets/add.png')}
-                        />
-                    </View>
-                </View>
-                <View style={{ flexDirection: 'row', margin: 5, backgroundColor:  "#FFE50061", height: 60 }}>
-                    <View style={{ justifyContent: 'center', alignItems: 'center',  width: 100 }}>
-                        <Image
-                        source={require('../../assets/breakfast.png')}
-                    />
-                    </View>
-                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start' }}>
-                        <Text>Breakfast</Text>
-                    </View>
-                    <View style={{justifyContent: 'center', alignItems: 'center' }}>
-                        <Image 
-                        source={require('../../assets/add.png')}
-                        />
-                    </View>
-                </View>
-                <View style={{ flexDirection: 'row', margin: 5, backgroundColor:  "#FFE50061", height: 60 }}>
-                    <View style={{ justifyContent: 'center', alignItems: 'center',  width: 100 }}>
-                        <Image
-                        source={require('../../assets/breakfast.png')}
-                    />
-                    </View>
-                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start' }}>
-                        <Text>Breakfast</Text>
-                    </View>
-                    <View style={{justifyContent: 'center', alignItems: 'center' }}>
-                        <Image 
-                        source={require('../../assets/add.png')}
-                        />
-                    </View>
-                </View>
-                <View style={{ flexDirection: 'row', margin: 5, backgroundColor:  "#FFE50061", height: 60 }}>
-                    <View style={{ justifyContent: 'center', alignItems: 'center',  width: 100 }}>
-                        <Image
-                        source={require('../../assets/breakfast.png')}
-                    />
-                    </View>
-                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start' }}>
-                        <Text>Breakfast</Text>
-                    </View>
-                    <View style={{justifyContent: 'center', alignItems: 'center' }}>
-                        <Image 
-                        source={require('../../assets/add.png')}
-                        />
-                    </View>
-                </View>
-                <View style={{ flexDirection: 'row', margin: 5, backgroundColor:  "#FFE50061", height: 60 }}>
-                    <View style={{ justifyContent: 'center', alignItems: 'center',  width: 100 }}>
-                        <Image
-                        source={require('../../assets/breakfast.png')}
-                    />
-                    </View>
-                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start' }}>
-                        <Text>Breakfast</Text>
-                    </View>
-                    <View style={{justifyContent: 'center', alignItems: 'center' }}>
-                        <Image 
-                        source={require('../../assets/add.png')}
-                        />
-                    </View>
-                </View>
-                <View style={{ flexDirection: 'row', margin: 5, backgroundColor:  "#FFE50061", height: 60 }}>
-                    <View style={{ justifyContent: 'center', alignItems: 'center',  width: 100 }}>
-                        <Image
-                        source={require('../../assets/breakfast.png')}
-                    />
-                    </View>
-                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start' }}>
-                        <Text>Breakfast</Text>
-                    </View>
-                    <View style={{justifyContent: 'center', alignItems: 'center' }}>
-                        <Image 
-                        source={require('../../assets/add.png')}
-                        />
-                    </View>
-                </View>
-                <View style={{ flexDirection: 'row', margin: 5, backgroundColor:  "#FFE50061", height: 60 }}>
-                    <View style={{ justifyContent: 'center', alignItems: 'center',  width: 100 }}>
-                        <Image
-                        source={require('../../assets/breakfast.png')}
-                    />
-                    </View>
-                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start' }}>
-                        <Text>Breakfast</Text>
-                    </View>
-                    <View style={{justifyContent: 'center', alignItems: 'center' }}>
-                        <Image 
-                        source={require('../../assets/add.png')}
-                        />
-                    </View>
-                </View>
-                <View style={{ flexDirection: 'row', margin: 5, backgroundColor:  "#FFE50061", height: 60 }}>
-                    <View style={{ justifyContent: 'center', alignItems: 'center',  width: 100 }}>
-                        <Image
-                        source={require('../../assets/breakfast.png')}
-                    />
-                    </View>
-                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start' }}>
-                        <Text>Breakfast</Text>
-                    </View>
-                    <View style={{justifyContent: 'center', alignItems: 'center' }}>
-                        <Image 
-                        source={require('../../assets/add.png')}
-                        />
-                    </View>
-                </View>
-                <View style={{ flexDirection: 'row', margin: 5, backgroundColor:  "#FFE50061", height: 60 }}>
-                    <View style={{ justifyContent: 'center', alignItems: 'center',  width: 100 }}>
-                        <Image
-                        source={require('../../assets/breakfast.png')}
-                    />
-                    </View>
-                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start' }}>
-                        <Text>Breakfast</Text>
-                    </View>
-                    <View style={{justifyContent: 'center', alignItems: 'center' }}>
-                        <Image 
-                        source={require('../../assets/add.png')}
-                        />
-                    </View>
-                </View>
-                <View style={{ flexDirection: 'row', margin: 5, backgroundColor:  "#FFE50061", height: 60 }}>
-                    <View style={{ justifyContent: 'center', alignItems: 'center',  width: 100 }}>
-                        <Image
-                        source={require('../../assets/breakfast.png')}
-                    />
-                    </View>
-                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start' }}>
-                        <Text>Breakfast</Text>
-                    </View>
-                    <View style={{justifyContent: 'center', alignItems: 'center' }}>
-                        <Image 
-                        source={require('../../assets/add.png')}
-                        />
-                    </View>
-                </View>
-                <View style={{ flexDirection: 'row', margin: 5, backgroundColor:  "#FFE50061", height: 60 }}>
-                    <View style={{ justifyContent: 'center', alignItems: 'center',  width: 100 }}>
-                        <Image
-                        source={require('../../assets/breakfast.png')}
-                    />
-                    </View>
-                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start' }}>
-                        <Text>Breakfast</Text>
-                    </View>
-                    <View style={{justifyContent: 'center', alignItems: 'center' }}>
-                        <Image 
-                        source={require('../../assets/add.png')}
-                        />
-                    </View>
-                </View>
-                <View style={{ flexDirection: 'row', margin: 5, backgroundColor:  "#FFE50061", height: 60 }}>
-                    <View style={{ justifyContent: 'center', alignItems: 'center',  width: 100 }}>
-                        <Image
-                        source={require('../../assets/breakfast.png')}
-                    />
-                    </View>
-                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start' }}>
-                        <Text>Breakfast</Text>
-                    </View>
-                    <View style={{justifyContent: 'center', alignItems: 'center' }}>
-                        <Image 
-                        source={require('../../assets/add.png')}
-                        />
-                    </View>
-                </View>
+                {dummy()}
             </ScrollView>
             </View>
         <View style={{ flex: 1, justifyContent: 'flex-end', alignItems: 'center' }}>
